04e1e905e88de9e6e53df5f7e580515fff512cbd
slobo10 <97814567+slobo10@users.noreply.github.com>
2023-07-17 16:47:44 -0700

    Create Expo app

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>React-Native-Mario-Bros has not been made yet. Please check in later when I've got a chance to make it.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
