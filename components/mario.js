271347c989c40cd7facbf522542de907e7bc1180
slobo10 <97814567+slobo10@users.noreply.github.com>
2023-08-09 17:38:08 -0700

    Make Mario's collision calculations a library

--- components/mario.js
+++ components/mario.js
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import { gameContext } from "../App";
+import { checkForCollision } from "../lib/mathlib";
 
 const Mario = () => {
     const stats = useRef({
@@ -52,31 +53,15 @@ const Mario = () => {
             ySpeed.current -= stats.current.gravity;
             y += ySpeed.current;
 
-            for (var i in bricks.current){
-                if (y >= bricks.current[i].posision[1] + 1 + ySpeed.current && y <= bricks.current[i].posision[1] + 1 && x > bricks.current[i].posision[0] - 1 && x < bricks.current[i].posision[0] + (bricks.current[i].dim !== undefined ? bricks.current[i].dim[0] : 1)){
-                    canJump.current = true;
-                    ySpeed.current = 0;
-                    y = bricks.current[i].posision[1] + 1;
-                    break;
-                } else if (y >= bricks.current[i].posision[1] - (bricks.current[i].dim !== undefined ? bricks.current[i].dim[1] : 1) && y <= bricks.current[i].posision[1] - (bricks.current[i].dim !== undefined ? bricks.current[i].dim[1] : 1) + ySpeed.current && x > bricks.current[i].posision[0] - 1 && x < bricks.current[i].posision[0] + (bricks.current[i].dim !== undefined ? bricks.current[i].dim[0] : 1)){
-                    ySpeed.current *= -1;
-                    y = bricks.current[i].posision[1] - (bricks.current[i].dim !== undefined ? bricks.current[i].dim[1] : 1);
-                    if (bricks.current[i].type === 'question'){
-                        bricks.current[i].type = 'empty';
-                    };
-                    break;
+            [x,y] = checkForCollision(x, y, xSpeed.current * stats.current.speed / stats.current.updateRate, ySpeed.current, bricks.current, (brick) => {
+                canJump.current = true;
+                ySpeed.current = 0;
+            }, (brick) => {
+                ySpeed.current *= -1;
+                if (brick.type === 'question'){
+                    brick.type = 'empty';
                 };
-            };
-
-            for (var i in bricks.current){
-                if (x >= bricks.current[i].posision[0] + (bricks.current[i].dim !== undefined ? bricks.current[i].dim[0] : 1) + xSpeed.current * stats.current.speed / stats.current.updateRate && x <= bricks.current[i].posision[0] + (bricks.current[i].dim !== undefined ? bricks.current[i].dim[0] : 1) && y > bricks.current[i].posision[1] - (bricks.current[i].dim !== undefined ? bricks.current[i].dim[1] : 1) && y < bricks.current[i].posision[1] + 1){
-                    x = bricks.current[i].posision[0] + (bricks.current[i].dim !== undefined ? bricks.current[i].dim[0] : 1);
-                    break;
-                } else if (x >= bricks.current[i].posision[0] - 1 && x <= bricks.current[i].posision[0] + xSpeed.current * stats.current.speed / stats.current.updateRate && y > bricks.current[i].posision[1] - (bricks.current[i].dim !== undefined ? bricks.current[i].dim[1] : 1) && y < bricks.current[i].posision[1] + 1){
-                    x = bricks.current[i].posision[0] - 1;
-                    break;
-                };
-            };
+            }, () => {}, () => {});
 
             setY(y);
             setX(x);
