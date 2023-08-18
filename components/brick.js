81b23a9a11b0af9672616a273c2de6d9a634948c
slobo10 <97814567+slobo10@users.noreply.github.com>
2023-08-17 18:41:19 -0700

    Add level scroller

--- components/brick.js
+++ components/brick.js
@@ -2,8 +2,11 @@ import { useContext, useEffect, useRef, useState } from "react";
 import { gameContext } from "../App";
 
 const Brick = (props) => {
-    var currentBrick = useRef(useContext(gameContext).level.bricks[props.id])
-    var [brickType, setBrickType] = useState(currentBrick.current.type)
+    var scrollDistanceRef = useRef(useContext(gameContext).game.scrollDistance);
+    var currentBrick = useRef(useContext(gameContext).level.bricks[props.id]);
+
+    var [scrollDistanceState, setScrollDistanceState] = useState(scrollDistanceRef.current.current);
+    var [brickType, setBrickType] = useState(currentBrick.current.type);
 
     useEffect(() => {
         if (brickType === 'question' || 'hidden') {
@@ -14,6 +17,12 @@ const Brick = (props) => {
                 };
             }, 50);
         };
+        setInterval(() => {
+            if (scrollDistanceRef.current.current != scrollDistanceState) {
+                scrollDistanceState = scrollDistanceRef.current.current;
+                setScrollDistanceState(scrollDistanceState);
+            };
+        })
     }, []);
 
     console.warn('Brick ' + props.id + ' rendered!');
@@ -21,12 +30,12 @@ const Brick = (props) => {
     switch (brickType) {
         case 'ground': {
             return(<>
-                <rect x={currentBrick.current.posision[0] * useContext(gameContext).game.gridSize} y={useContext(gameContext).game.height - (currentBrick.current.posision[1] + 1) * useContext(gameContext).game.gridSize} width={useContext(gameContext).game.gridSize * currentBrick.current.dim[0]} height={useContext(gameContext).game.gridSize * currentBrick.current.dim[1]} fill="#5c260f" />
+                <rect x={(currentBrick.current.posision[0] - scrollDistanceState) * useContext(gameContext).game.gridSize} y={useContext(gameContext).game.height - (currentBrick.current.posision[1] + 1) * useContext(gameContext).game.gridSize} width={useContext(gameContext).game.gridSize * currentBrick.current.dim[0]} height={useContext(gameContext).game.gridSize * currentBrick.current.dim[1]} fill="#5c260f" />
             </>);
         };
         case 'question': {
             return(<>
-                <rect x={currentBrick.current.posision[0] * useContext(gameContext).game.gridSize} y={useContext(gameContext).game.height - (currentBrick.current.posision[1] + 1) * useContext(gameContext).game.gridSize} width={useContext(gameContext).game.gridSize} height={useContext(gameContext).game.gridSize} fill="yellow" /><text fontSize={100} x={currentBrick.current.posision[0] * useContext(gameContext).game.gridSize + 15} y={useContext(gameContext).game.height - currentBrick.current.posision[1] * useContext(gameContext).game.gridSize - 2}>?</text>
+                <rect x={(currentBrick.current.posision[0] - scrollDistanceState) * useContext(gameContext).game.gridSize} y={useContext(gameContext).game.height - (currentBrick.current.posision[1] + 1) * useContext(gameContext).game.gridSize} width={useContext(gameContext).game.gridSize} height={useContext(gameContext).game.gridSize} fill="yellow" /><text fontSize={100} x={(currentBrick.current.posision[0] - scrollDistanceRef.current.current) * useContext(gameContext).game.gridSize + 15} y={useContext(gameContext).game.height - currentBrick.current.posision[1] * useContext(gameContext).game.gridSize - 2}>?</text>
             </>);
         };
         case 'hidden': {
@@ -34,7 +43,7 @@ const Brick = (props) => {
         };
         case 'empty': {
             return(<>
-                <rect x={currentBrick.current.posision[0] * useContext(gameContext).game.gridSize} y={useContext(gameContext).game.height - (currentBrick.current.posision[1] + 1) * useContext(gameContext).game.gridSize} width={useContext(gameContext).game.gridSize} height={useContext(gameContext).game.gridSize} fill="#5c260f" />
+                <rect x={(currentBrick.current.posision[0] - scrollDistanceState) * useContext(gameContext).game.gridSize} y={useContext(gameContext).game.height - (currentBrick.current.posision[1] + 1) * useContext(gameContext).game.gridSize} width={useContext(gameContext).game.gridSize} height={useContext(gameContext).game.gridSize} fill="#5c260f" />
             </>);
         };
     };
